f2963893acb6e7dc53b54b9c84a9bc057f277b27
Erica Ford <erica.ford0205@gmail.com>
2024-07-11 13:38:24 -0500

    begin testing input selection

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// Target form
+const budgetForm = document.forms["budget-form"];
+
+budgetForm.addEventListener("submit", function (e) {
+  //prevent form from being sent
+  e.preventDefault();
+
+  let USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const description = budgetForm.querySelector('input[type="text"]').value;
+
+  const amount = parseFloat(
+    budgetForm.querySelector('input[type="number"').value
+  );
+
+  const date = budgetForm.querySelector('input[type="date"]').value;
+
+  console.log(`Description: ${description}`);
+  console.log(`Amount: ${USDollar.format(amount)}`);
+  console.log(`Date: ${date}`);
+});
