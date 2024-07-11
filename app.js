a86cb97ef5baef368db8870aa91e19f510d46c65
Erica Ford <erica.ford0205@gmail.com>
2024-07-11 15:08:15 -0500

    add function to add table rows to the expense table

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 "use strict";
+// Keep track of new Rows
+let currentIndex = 0;
+// Set up US currency formatter
+let USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 // Target form
 const budgetForm = document.forms["budget-form"];
 
 budgetForm.addEventListener("submit", function (e) {
-  //prevent form from being sent
+  // Prevent form from being sent
   e.preventDefault();
 
-  let USDollar = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-
+  // Select input values
   const description = budgetForm.querySelector('input[type="text"]').value;
-
   const amount = parseFloat(
     budgetForm.querySelector('input[type="number"').value
-  );
-
+  ).toFixed(2);
   const date = budgetForm.querySelector('input[type="date"]').value;
 
-  console.log(`Description: ${description}`);
-  console.log(`Amount: ${USDollar.format(amount)}`);
-  console.log(`Date: ${date}`);
+  // Add table row and cells
+  addTableRow(description, amount, date);
 });
+
+// Add row when budget form is submitted
+let addTableRow = (value, amount, date) => {
+  let expenseTable = document.getElementById("expense-table");
+  let row = expenseTable.insertRow(currentIndex);
+  let cellDesc = row.insertCell(0);
+  let cellAmt = row.insertCell(1);
+  let cellDate = row.insertCell(2);
+
+  cellDesc.innerHTML = value;
+  cellAmt.innerHTML = amount;
+  cellDate.innerHTML = date;
+
+  // Add class of income or expense to new table row
+  amount > 0 ? row.classList.add("income") : row.classList.add("expense");
+
+  currentIndex++;
+};
